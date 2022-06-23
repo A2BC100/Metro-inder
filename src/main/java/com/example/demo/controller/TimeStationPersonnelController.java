226acb3e86f78d9dae918d48bd2eb98b72e6c518
choijaehak <cms893811@naver.com>
2022-06-23 22:25:06 +0900

    commit

---
+++ src/main/java/com/example/demo/controller/TimeStationPersonnelController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.TimeStationPersonnelService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+@Controller
+@AllArgsConstructor
+public class TimeStationPersonnelController {
+    TimeStationPersonnelService timeStationPersonnelService;
+    @GetMapping("/seoulSubwayTimeZoneInformationSave")
+    public void test() throws IOException {
+        timeStationPersonnelService.peopleInformationBySeoulAtTime();
+    }
+}
--- src/main/java/com/example/demo/service/TimeStationPersonnelService.java
+++ src/main/java/com/example/demo/service/TimeStationPersonnelService.java
@@ -1,6 +1,8 @@
 package com.example.demo.service;
 
 
+import com.example.demo.repository.TimeStationPersonnelRepository;
+import lombok.AllArgsConstructor;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -19,35 +21,35 @@ import java.util.List;
 
 @Component
 @Service
-public class ApiService {
-
+@AllArgsConstructor
+public class TimeStationPersonnelService {
     @Value("${generalKey}")
     public String generalKey;
     @Value("${realTimeKey}")
     public String realTimeKey;
+    TimeStationPersonnelRepository timeStationPersonnelRepository;
 
     // 서울시 지하철 호선별 역별 시간대별 승하차 인원 정보
-    public void peopleInformationBySeoulAtTime() throws IOException {
+    public void peopleInformationBySeoulAtTime() {
         try {
             StringBuilder urlBuilder = new StringBuilder("http://openapi.seoul.go.kr:8088");
             urlBuilder.append("/" + URLEncoder.encode(generalKey, "UTF-8"));
             urlBuilder.append("/" + URLEncoder.encode("json", "UTF-8"));
-            urlBuilder.append("/" + URLEncoder.encode("CardSubwayTime", "UTF-8")); /*서비스명, 대소문자 구분*/
+            urlBuilder.append("/" + URLEncoder.encode("CardSubwayTime", "UTF-8"));
             urlBuilder.append("/" + URLEncoder.encode("1", "UTF-8"));
-            urlBuilder.append("/" + URLEncoder.encode("1000", "UTF-8"));
-            // 상위 5개는 필수적으로 순서바꾸지 않고 호출해야 합니다.
+            urlBuilder.append("/" + URLEncoder.encode("5", "UTF-8"));
 
-            /* 서비스별 추가 요청인자들*/
+            /* 서비스별 추가 요청인자*/
             urlBuilder.append("/" + URLEncoder.encode("202205", "UTF-8"));//월별, 최신 2022년 5월까지
 
             URL url = new URL(urlBuilder.toString());
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Content-type", "application/json");
-            System.out.println("Response code: " + conn.getResponseCode()); /* 연결 자체에 대한 확인이 필요하므로 추가합니다.*/
+            System.out.println("Response code: " + conn.getResponseCode()); /* 연결에 대한 확인*/
             BufferedReader rd;
 
-            // 서비스코드가 정상이면 200~300사이의 숫자가 나옵니다.
+            // 서비스코드가 정상이면 200~300사이의 숫자
             if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
                 rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
             } else {
@@ -66,18 +68,40 @@ public class ApiService {
             JSONObject cardSubwayTime = (JSONObject) jsonObject.get("CardSubwayTime");
 
             int listTotalCount = Long.valueOf((Long) cardSubwayTime.get("list_total_count")).intValue() ;
-            if(listTotalCount > 1000) {
-
-            }
+//            if(listTotalCount > 1000) {
+//
+//            }
 
             JSONArray jsonArr = (JSONArray) cardSubwayTime.get("row");
-            System.out.println(cardSubwayTime);
-            System.out.println(listTotalCount);
             List<List<JSONObject>> jsonSameStationList = new ArrayList<>();
             for(int i=0;i<jsonArr.size();i++){
                 for(int j = 0; j<jsonArr.size(); j++) {
                     JSONObject row = (JSONObject)jsonArr.get(i);
                     String subStaNm = (String)row.get("SUB_STA_NM");
+                    int oneRide = (int) Math.round((double)row.get("ONE_RIDE_NUM"));
+                    int twoRide = (int) Math.round((double)row.get("TWO_RIDE_NUM"));
+                    int threeRide = (int) Math.round((double)row.get("THREE_RIDE_NUM"));
+                    int fourRide = (int) Math.round((double)row.get("FOUR_RIDE_NUM"));
+                    int fiveRide = (int) Math.round((double)row.get("FIVE_RIDE_NUM"));
+                    int sixRide = (int) Math.round((double)row.get("SIX_RIDE_NUM"));
+                    int sevenRide = (int) Math.round((double)row.get("SEVEN_RIDE_NUM"));
+                    int eightRide = (int) Math.round((double)row.get("EIGHT_RIDE_NUM"));
+                    int nineRide = (int) Math.round((double)row.get("NINE_RIDE_NUM"));
+                    int tenRide = (int) Math.round((double)row.get("TEN_RIDE_NUM"));
+                    int elevenRide = (int) Math.round((double)row.get("ELEVEN_RIDE_NUM"));
+                    int twelveRide = (int) Math.round((double)row.get("TWELVE_RIDE_NUM"));
+                    int thirteenRide = (int) Math.round((double)row.get("THIRTEEN_RIDE_NUM"));
+                    int fourteenRide = (int) Math.round((double)row.get("FOURTEEN_RIDE_NUM"));
+                    int fifteenRide = (int) Math.round((double)row.get("FIFTEEN_RIDE_NUM"));
+                    int sixteenRide = (int) Math.round((double)row.get("SIXTEEN_RIDE_NUM"));
+                    int seventeenRide = (int) Math.round((double)row.get("SEVENTEEN_RIDE_NUM"));
+                    int eighteenRide = (int) Math.round((double)row.get("EIGHTEEN_RIDE_NUM"));
+                    int nineteenRide = (int) Math.round((double)row.get("NINETEEN_RIDE_NUM"));
+                    int twentyRide = (int) Math.round((double)row.get("TWENTY_RIDE_NUM"));
+                    int twentyoneRide = (int) Math.round((double)row.get("TWENTY_ONE_RIDE_NUM"));
+                    int twentytwoRide = (int) Math.round((double)row.get("TWENTY_TWO_RIDE_NUM"));
+                    int twentythreeRide = (int) Math.round((double)row.get("TWENTY_THREE_RIDE_NUM"));
+                    int midnightRide = (int) Math.round((double)row.get("MIDNIGHT_RIDE_NUM"));
 
                 }
 
