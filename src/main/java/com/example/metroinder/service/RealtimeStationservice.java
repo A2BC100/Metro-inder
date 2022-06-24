2afc6b770cb4c0f3f6078dbe54f2390aba496930
JungGon Lee <dlwndrhs05@gmail.com>
2022-06-24 11:59:12 +0900

    Realtimestaion api test commit

--- src/main/java/com/example/metroinder/service/RealtimeStationservice.java
+++ src/main/java/com/example/metroinder/service/RealtimeStationservice.java
@@ -20,34 +20,34 @@ public class RealtimeStationservice {
     public String realTimeKey;
 
     public String RealtimeStaion() throws IOException {
-        StringBuilder urlBuilder = new StringBuilder("http://openapi.seoul.go.kr:8088");
+        StringBuilder urlBuilder = new StringBuilder("http://openapi.seoul.go.kr");
         urlBuilder.append("/" + URLEncoder.encode("api","UTF-8"));
         urlBuilder.append("/" + URLEncoder.encode("subway","UTF-8"));
         urlBuilder.append("/" + URLEncoder.encode(realTimeKey, "UTF-8"));
         urlBuilder.append("/" + URLEncoder.encode("json", "UTF-8"));
         urlBuilder.append("/" + URLEncoder.encode("realtimeStationArrival/ALL", "UTF-8"));
         urlBuilder.append("/" + URLEncoder.encode("1", "UTF-8"));
-        urlBuilder.append("/" + URLEncoder.encode("5", "UTF-8"));
+        urlBuilder.append("/" + URLEncoder.encode("2", "UTF-8"));
 
 
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-type", "application/json");
-        System.out.println("Response code: " + conn.getResponseCode());
-        BufferedReader rd;
+        conn.setRequestProperty("Content-type","application/json");
+        System.out.println("Response cod:"+conn.getResponseCode());
+        BufferedReader Br;
 
         if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            Br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
         } else {
-            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            Br = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
         }
         StringBuilder sb = new StringBuilder();
         String line;
-        while ((line = rd.readLine()) != null) {
+        while ((line = Br.readLine()) != null) {
             sb.append(line);
         }
-        rd.close();
+        Br.close();
         conn.disconnect();
         System.out.println(sb.toString());
 
