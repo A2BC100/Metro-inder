bdae2927f15ab4cc905e02ce80143e9706d68e8d
choijaehak <cms893811@naver.com>
2024-01-29 17:07:15 +0900

    구글맵 인기 시간대 정보(혼잡도 정보) 받아오는 코드 테스트 롤백

--- src/main/java/com/example/metroinder/congestion/controller/TimeStationPersonnelController.java
+++ src/main/java/com/example/metroinder/congestion/controller/TimeStationPersonnelController.java
@@ -1,6 +1,4 @@
 package com.example.metroinder.congestion.controller;
-import com.example.metroinder.congestion.model.PopularTimeInfo;
-import com.example.metroinder.congestion.service.PopularTimeService;
 import com.example.metroinder.congestion.service.TimeStationPersonnelService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-;
 import java.util.Map;
 
 @Slf4j
@@ -18,7 +15,6 @@ import java.util.Map;
 @AllArgsConstructor
 public class TimeStationPersonnelController {
     private final TimeStationPersonnelService timeStationPersonnelService;
-    private final PopularTimeService popularTimeService;
 
     @GetMapping("/returnPeopleCount")
     @ResponseBody
@@ -27,38 +23,4 @@ public class TimeStationPersonnelController {
         return new ResponseEntity<>(json, HttpStatus.OK);
     }
 
-    @GetMapping("/test")
-    public ResponseEntity<Object> returnPopularTimeData(/*@RequestParam("stationName") String stationName*/) {
-        PopularTimeInfo popularTimeInfo = popularTimeService.downloadCurrentTraffic("명학역");
-        if (popularTimeInfo != null){
-            log.info("역이름 : " + popularTimeInfo.station);
-            log.info("시간대 : " + popularTimeInfo.time);
-            log.info("붐빔도 : " + popularTimeInfo.peoplerate);
-        }
-        popularTimeInfo = popularTimeService.downloadCurrentTraffic("금정역");
-        if (popularTimeInfo != null){
-            log.info("역이름 : " + popularTimeInfo.station);
-            log.info("시간대 : " + popularTimeInfo.time);
-            log.info("붐빔도 : " + popularTimeInfo.peoplerate);
-        }
-        popularTimeInfo = popularTimeService.downloadCurrentTraffic("구로역");
-        if (popularTimeInfo != null){
-            log.info("역이름 : " + popularTimeInfo.station);
-            log.info("시간대 : " + popularTimeInfo.time);
-            log.info("붐빔도 : " + popularTimeInfo.peoplerate);
-        }
-        popularTimeInfo = popularTimeService.downloadCurrentTraffic("구파발역");
-        if (popularTimeInfo != null){
-            log.info("역이름 : " + popularTimeInfo.station);
-            log.info("시간대 : " + popularTimeInfo.time);
-            log.info("붐빔도 : " + popularTimeInfo.peoplerate);
-        }
-        popularTimeInfo = popularTimeService.downloadCurrentTraffic("안양역");
-        if (popularTimeInfo != null){
-            log.info("역이름 : " + popularTimeInfo.station);
-            log.info("시간대 : " + popularTimeInfo.time);
-            log.info("붐빔도 : " + popularTimeInfo.peoplerate);
-        }
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
 }
